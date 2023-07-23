7cd770e53040ac8b3ffdc979ee8c0a760b423ed5
Luis Alberto Perez Coello <luisalbertodev@gmail.com>
2023-07-22 19:22:56 -0600

    initial commit

---
+++ src/views/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+	return <div className={styles.content}>Contact</div>;
+};
+
+export default Contact;
